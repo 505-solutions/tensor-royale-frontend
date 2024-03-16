404df39fd7a16cd34008aaa1b33396f0dfd05566
Luka Skeledzija <36110818+lukaske@users.noreply.github.com>
2024-03-16 23:39:06 +0100

    update file upload

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,8 @@ import { ProblemDetailComponent } from './components/problems/ProblemDetailCompo
 import { AddProblemSubmissionComponent } from './components/problems/AddProblemSubmissionComponent';
 import { AddModelComponent } from './components/models/AddModelComponent';
 import FilecoinUpload from './components/filecoinUpload';
+import { EthersExtension } from "@dynamic-labs/ethers-v5";
+import { DynamicWagmiConnector } from "@dynamic-labs/wagmi-connector";
 
 import { DynamicWidget, DynamicContextProvider } from '@dynamic-labs/sdk-react-core';
 
@@ -44,19 +46,24 @@ const evmNetworks = [
 export default function App() {
   return (
     <DynamicContextProvider
-      settings={{
-        environmentId: '7af57577-b9bb-4503-b0c7-27da15986c8a',
-        walletConnectors: [EthereumWalletConnectors, StarknetWalletConnectors],
-        evmNetworks: evmNetworks,
-      }}
+    settings={{
+      environmentId: "7af57577-b9bb-4503-b0c7-27da15986c8a",
+      walletConnectors: [EthereumWalletConnectors, StarknetWalletConnectors],
+      walletConnectorExtensions: [EthersExtension],
+      evmNetworks: evmNetworks,
+    }}
     >
-      <MantineProvider theme={theme}>
-        <AppShell>
-          <AppShell.Header>
-            <Group h="70px" px="md" justify="space-between">
-              <Text size="xl" fw={700}>
-                TensorRoyale
-              </Text>
+    <DynamicWagmiConnector>
+
+
+    <MantineProvider theme={theme}>
+      <AppShell>
+
+        <AppShell.Header>
+          <Group h="70px" px="md" justify="space-between">
+            <Text size="xl" fw={700}>
+              TensorRoyale
+            </Text>
 
               <Group justify="center">
                 <NavLink to="/">
@@ -97,47 +104,36 @@ export default function App() {
             </Group>
           </AppShell.Header>
 
-          <AppShell.Main pt="70px">
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/problems" element={<ProblemsPage />} />
-              <Route path="/data-sets" element={<DataSetsPage />} />
-              <Route path="/models" element={<ModelsPage />} />
-              <Route path="/leaderboard" element={<LeaderboardPage />} />
-              <Route path="/problems/add" element={<AddProblemComponent />} />
-              <Route
-                path="/problems/add-submission/:id"
-                element={<AddProblemSubmissionComponent />}
-              />
-              <Route path="/models/add" element={<AddModelComponent />} />
-              <Route
-                path="/data-sets/add"
-                element={
-                  <Center w="100vw">
-                    <Flex w="70vw" direction="column">
-                      <AddDatasetComponent displayTitle />
-                    </Flex>
-                  </Center>
-                }
-              />
-              <Route
-                path="/data-sets/add/:id"
-                element={
-                  <Center w="100vw">
-                    <Flex w="70vw" direction="column">
-                      <AddDatasetComponent displayTitle />
-                    </Flex>
-                  </Center>
-                }
-              />
-              <Route path="/problems/detail/:id" element={<ProblemDetailComponent />}></Route>
-              <Route path="/filecoin" element={<FilecoinUpload />} />
-              <Route path="/profile" element={<ProfilePage />} />
+        <AppShell.Main pt="70px">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/problems" element={<ProblemsPage />} />
+            <Route path="/data-sets" element={<DataSetsPage />} />
+            <Route path="/models" element={<ModelsPage />} />
+            <Route path="/leaderboard" element={<LeaderboardPage />} />
+            <Route path="/problems/add" element={<AddProblemComponent />} />
+            <Route path="/problems/add-submission/:id" element={<AddProblemSubmissionComponent />} />
+            <Route path="/models/add" element={<AddModelComponent />} />
+            <Route
+              path="/data-sets/add"
+              element={
+                <Center w="100vw">
+                  <Flex w="70vw" direction="column">
+                    <AddDatasetComponent displayTitle />
+                  </Flex>
+                </Center>
+              }
+            />
+            <Route path="/problems/detail/:id" element={<ProblemDetailComponent />}></Route>
+            <Route path="/filecoin" element={<FilecoinUpload />} />
+            <Route path="/profile" element={<ProfilePage />} />
 
-            </Routes>
-          </AppShell.Main>
-        </AppShell>
-      </MantineProvider>
-    </DynamicContextProvider>
+          </Routes>
+        </AppShell.Main>
+      </AppShell>
+    </MantineProvider>
+    </DynamicWagmiConnector>
+  </DynamicContextProvider>
+
   );
 }
--- src/components/filecoinUpload.tsx
+++ src/components/filecoinUpload.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import lighthouse from '@lighthouse-web3/sdk'
-import { Button, FileInput, Progress, Stack } from '@mantine/core';
+import { Button, FileInput, Progress, Stack, Code, List, ListItem, ThemeIcon, Text } from '@mantine/core';
+import {IconFile} from  '@tabler/icons-react';
 
 function App() {
     const [value, setValue] = useState<File[]>([]);
@@ -8,16 +9,29 @@ function App() {
     const [progress, setProgress] = useState<number>(0);
     const [output, setOutput] = useState<any>([]);
     const [priceEstimate, setPriceEstimate] = useState<BigInt>(BigInt(0));
+    const [apiToken, setApiToken] = useState<string>("");
+    const [loading, setLoading] = useState<boolean>(false);
+    const [stepIndex, setStepIndex] = useState<number>(0);
+
+    const steps = [
+      'Select files and click "Upload"',
+      'Pay and wait for Filecoin storage provisioning...',
+      'Sign message to verify wallet ownership...',
+      'Uploading to Filecoin...',
+      'Upload complete ✅'
+    ]
+
+    const estimatePrice = async () => {
+      let price = await lighthouse.getPrice(value, "calibration", "usdc")
+      console.log(price)
+      const regex = /\d+/g;
+      const numericPart = price.toString().match(regex)?.join('');
+      console.log(numericPart);
+      setPriceEstimate(BigInt(numericPart!) / BigInt("1000000000000000"));
+  }
+  
 
     useEffect(() => {
-        const estimatePrice = async () => {
-            let price = await lighthouse.getPrice(value, "calibration", "usdc")
-            console.log(price)
-            const regex = /\d+/g;
-            const numericPart = price.toString().match(regex)?.join('');
-            console.log(numericPart);
-            setPriceEstimate(BigInt(numericPart!) / BigInt("1000000000000000"));
-        }
         if (value.length > 0) {
             estimatePrice();
         }
@@ -32,15 +46,36 @@ function App() {
   }
 
   const uploadFile = async(file: any) =>{
-    console.log(file)
-    // Push file to lighthouse node
-    // Both file and folder are supported by upload function
-    // Third parameter is for multiple files, if multiple files are to be uploaded at once make it true
-    // Fourth parameter is the deal parameters, default null
-    const output = await lighthouse.upload(file, "4414e711.bdd4d86b458941e98806bcfbb1f7d396", true, undefined, progressCallback)
-    console.log('File Status:', output.data[-1])
-    setProgress(100)
-    /*
+    try {
+      setLoading(true);
+      setStepIndex(1);
+      let fil_price: bigint = await lighthouse.getPrice(value, "calibration", "native");
+      let tx = await lighthouse.fund(fil_price, "calibration", "native");
+      setStepIndex(2);
+      let authToken = await lighthouse.oneTimeAuth();
+      setApiToken(authToken);
+      setStepIndex(3);
+      console.log(tx);
+      console.log(file);
+      // Push file to lighthouse node
+      // Both file and folder are supported by upload function
+      // Third parameter is for multiple files, if multiple files are to be uploaded at once make it true
+      // Fourth parameter is the deal parameters, default null
+      //authToken = "4414e711.bdd4d86b458941e98806bcfbb1f7d396";
+      const output = await lighthouse.upload(file, authToken, true, undefined, progressCallback);
+      setOutput(output);
+      console.log('File Status:', output);
+      setProgress(100);
+      setStepIndex(4);
+      setLoading(false);
+      const response = await lighthouse.getUploads(authToken)
+      console.log(response)
+    
+  } catch (error) {
+      console.error('Error:', error);
+      setLoading(false);
+  }
+      /*
       output:
         data: {
           Name: "filename.txt",
@@ -56,9 +91,24 @@ function App() {
     <div className="App" style={{maxWidth: 600, margin: 'auto', paddingTop: 50}}>
     <Stack>
             <FileInput label="Upload files" placeholder="Upload files" onChange={e => setValue(e)} multiple/>
-            <p>Estimated cost: {(Number(priceEstimate))/1000} USDC</p>
-            {!done? <Progress transitionDuration={500} value={progress} />: "Upload complete ✅"}
-            <Button onClick={() => uploadFile(value)} disabled={progress !== 0}>{!done? "Upload" : "Upload complete"}</Button>
+            {!done? <Progress transitionDuration={500} value={progress} />: ""}
+            
+            <Button loading={loading} onClick={() => uploadFile(value)} disabled={progress !== 0}>{!done? "Upload" : "Upload complete"}</Button>
+            {<Text style={{textAlign: 'center'}}><b>{steps[stepIndex]}</b><br></br><small>Estimated cost: {(Number(priceEstimate))/1000} USDC</small></Text>}
+            {output && output.data && <List
+                  spacing="xs"
+                  size="sm"
+                  center
+                  icon={
+                    <IconFile  />}
+            >{output.data.map(
+                (file: any, index: number) => (
+                    <ListItem key={index}>
+                        <Code><b>{file.Name || '< Parent folder >'}</b></Code> - <Code>{file.Hash} | <a href={`https://gateway.lighthouse.storage/ipfs/${file.Hash}`} target="_blank" rel="noreferrer">IPFS ↗</a> </Code>
+                    </ListItem>
+                )
+            )}</List>}
+
         </Stack>
 
     </div>
