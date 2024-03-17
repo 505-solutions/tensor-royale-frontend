8032525b5c98923a4c927b7dab8f438cdf9459ab
Luka Skeledzija <36110818+lukaske@users.noreply.github.com>
2024-03-17 02:05:11 +0100

    front

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,13 +1,27 @@
-import { Center, Image } from '@mantine/core';
+import { Center, Image, Text } from '@mantine/core';
+import ReactMarkdown from 'react-markdown'
 
 import logo from '../images/logo.jpg';
+
+const text: string = 
+`
+# TensorRoyale - ETHGlobal London 2024
+`
 
 export function HomePage() {
   return (
     <>
       <Center>
-        <Image src={logo} w="70vw" />
+        <Image style={{maxWidth: 350, padding: 15}} src={logo} w="100vw" />
       </Center>
-    </>
+      <Center >
+        <div style={{flexDirection: 'column', maxWidth: 600, margin: 'auto', textAlign: 'justify'}}>        <ReactMarkdown>
+        {text}
+
+        </ReactMarkdown>
+</div>
+        </Center>
+        
+        </>
   );
 }
